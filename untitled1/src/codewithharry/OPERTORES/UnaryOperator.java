863b13e6349eab4cec3c0f900a217a33d8b7b665
krushna giri <girikrushna8888@gmail.com>
2023-06-13 22:23:15 +0530

    Arithmatic operator pragram added

--- untitled1/src/codewithharry/OPERTORES/UnaryOperator.java
+++ untitled1/src/codewithharry/OPERTORES/UnaryOperator.java
@@ -4,11 +4,17 @@ public class UnaryOperator {
     public static void main(String[] args) {
         int x= 10;
 
-       // postfix
-    //    System.out.println(++x);
+
         System.out.println(x++);
-     System.out.println(++x);
-       // System.out.println(x++);
+        System.out.println(++x);
+        System.out.println(x--);
+        System.out.println(--x);
+
+        System.out.println(x++);
+        System.out.println(++x);
+        System.out.println(x--);
+        System.out.println(--x);
+
 
 
     }
