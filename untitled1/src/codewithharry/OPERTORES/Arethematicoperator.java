702f38ded122c23f0a265eb6050123f02c1ec59c
krushna giri <girikrushna8888@gmail.com>
2023-06-13 22:10:59 +0530

    Arithmatic operator

---
+++ untitled1/src/codewithharry/OPERTORES/Arethematicoperator.java
@@ -0,0 +1,24 @@
+package codewithharry.OPERTORES;
+
+public class Arethematicoperator {
+    public static void main(String[] args){
+        //Arethematic operator
+       // int a = 4;
+        // int b = 6 %a;
+       // System.out.println(b);
+
+        //assignment operator
+        int b=9;
+        b*=3;   // 9*3 = 27 output
+        System.out.println(b);
+
+
+        //comparison operator
+      //  System.out.println(50<5); // out put is false
+
+        //logical operator
+       // System.out.println(50>5 && 67<8);// out put is flase because true false= true
+        System.out.println(56>5 || 56>57);// out put is true beacuse true false = true
+    }
+
+}
---
+++ untitled1/src/codewithharry/OPERTORES/UnaryOperator.java
@@ -0,0 +1,15 @@
+package codewithharry.OPERTORES;
+
+public class UnaryOperator {
+    public static void main(String[] args) {
+        int x= 10;
+
+       // postfix
+    //    System.out.println(++x);
+        System.out.println(x++);
+     System.out.println(++x);
+       // System.out.println(x++);
+
+
+    }
+}
