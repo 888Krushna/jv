ca87b44b8cd57e75ba1207e53589ab54f0ade99e
krushna giri <girikrushna8888@gmail.com>
2023-06-19 12:05:50 +0530

    added new file java pratice datatype

--- untitled1/src/codewithharry/OPERTORES/UnaryOperator.java
+++ untitled1/src/codewithharry/OPERTORES/UnaryOperator.java
@@ -4,8 +4,9 @@ public class UnaryOperator {
     public static void main(String[] args) {
         int x= 10;
 
-
+         // postfix
         System.out.println(x++);
+        // prefix
         System.out.println(++x);
         System.out.println(x--);
         System.out.println(--x);
