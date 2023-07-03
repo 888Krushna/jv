031753c055d62758e918904a764e975936855d13
krushna giri <girikrushna8888@gmail.com>
2023-07-03 09:40:25 +0530

    oops start

---
+++ untitled1/src/codewithharry/Constructor/VD42_Constructor_default.java
@@ -0,0 +1,19 @@
+package codewithharry.Constructor;
+
+public class VD42_Constructor_default {
+    // java default constructor
+    //Java Program to create and call a default constructor
+    static class Bike {
+        //creating a default constructor
+        Bike() {
+            System.out.println("i got new bike ktm ");
+        }
+
+        //main method
+        public static void main(String[] args) {
+
+            //calling a default constructor
+            Bike ktm = new Bike();
+        }
+    }
+}
--- untitled1/src/codewithharry/OOPS/VD42_Constructor.java
+++
@@ -1,4 +0,0 @@
-package codewithharry.OOPS;
-
-public class VD42_Constructor {
-}
